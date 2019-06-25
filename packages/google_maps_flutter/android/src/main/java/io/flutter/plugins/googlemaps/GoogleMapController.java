f05413d432edcae78d0bd1cf347d558d1c4b6d79
Eimji <Eimji@users.noreply.github.com>
2019-06-25 15:40:51 +0200

    [Google maps] Fix vertical padding for Android myLocation button

--- packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
+++ packages/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapController.java
@@ -619,10 +619,7 @@ final class GoogleMapController
       if (myLocationButtonEnabled && myLocationButtonVerticalPadding > 0) {
         View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
         RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
-        // position on right bottom
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-        rlp.setMargins(0, myLocationButtonVerticalPadding, myLocationButtonVerticalPadding, 0);        
+        rlp.setMargins(0, (int) (density * (float) myLocationButtonVerticalPadding), rlp.rightMargin, 0);        
       } 
     } else {
       // TODO(amirh): Make the options update fail.
